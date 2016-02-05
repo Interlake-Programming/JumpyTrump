8abb8f96be970135badd6cdc235146d0ad5ba68f
Peachball <chen_xu@live.com>
2016-02-04 18:07:52 -0800

    Figured out how to draw to canvas using bitmaps

--- app/src/main/java/ihs/jumpytrump/StartActivity.java
+++ app/src/main/java/ihs/jumpytrump/StartActivity.java
@@ -1,21 +1,61 @@
 package ihs.jumpytrump;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class StartActivity extends Activity {
 
+    SurfaceHolder holder;
+    SurfaceView surfaceRef;
+    Bitmap trump;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        trump = BitmapFactory.decodeResource(getResources(), R.drawable.trump);
+        surfaceRef = (SurfaceView) findViewById(R.id.canvas);
+        holder = surfaceRef.getHolder();
+        holder.addCallback(new SurfaceHolder.Callback(){
+
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                Canvas canvas = holder.lockCanvas();
+                Paint p = new Paint();
+                p.setColor(Color.BLUE);
+                if(canvas == null){
+                }
+                else{
+                    canvas.drawBitmap(trump, 0, 0, p);
+                    holder.unlockCanvasAndPost(canvas);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
     }
 
     @Override
